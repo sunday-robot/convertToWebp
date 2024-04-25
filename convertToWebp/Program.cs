33cdcd58b4d6fe4cad76fbd68f3d0a14b7a73230
sunday_robot <sgx03150@nifty.com>
2024-04-26 01:49:30 +0900

    一応動作するようになった。

--- convertToWebp/Program.cs
+++ convertToWebp/Program.cs
@@ -1,24 +1,40 @@
-﻿using SixLabors.ImageSharp.Formats.Webp;
-using static System.Net.Mime.MediaTypeNames;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Webp;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         foreach (var arg in args)
             Convert(arg);
     }
 
-    static void Convert(String fileOrDirectoryPath)
+    static void Convert(string fileOrDirectoryPath)
     {
         if (File.Exists(fileOrDirectoryPath))
         {
-            ConvertFile(fileOrDirectoryPath);
+            var parentDirectoryPath = Path.GetDirectoryName(fileOrDirectoryPath);
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileOrDirectoryPath);
+            var destinationFilePath = Path.Combine(parentDirectoryPath, fileNameWithoutExtension + ".webp");
+            ConvertFile(fileOrDirectoryPath, destinationFilePath);
         }
         else if (Directory.Exists(fileOrDirectoryPath))
         {
-            ConvertDirectory(fileOrDirectoryPath);
+            // 引数に指定されたディレクトリ:
+            // a/
+            // 上記ディレクトリの中:
+            // a/b.jpg
+            // a/c/d.png
+            // a/c/e/f.png
+            // ↓
+            // 新規に以下のディレクトリを作成し、
+            // a.webp/
+            // 以下のファイルを作成する
+            // a.webp/b.webp
+            // a.webp/c/d.webp
+            // a.webp/c/e/f.webp
+            var destinationDirectoryPath = fileOrDirectoryPath + ".webp";
+            ConvertDirectory(fileOrDirectoryPath, destinationDirectoryPath);
         }
         else
         {
@@ -26,35 +42,47 @@ internal class Program
         }
     }
 
-    private static void ConvertDirectory(string fileOrDirectoryPath)
+    private static void ConvertDirectory(string sourceDirectoryPath, string destinationDirectoryPath)
     {
-        throw new NotImplementedException();
-    }
+        _ = Directory.CreateDirectory(destinationDirectoryPath);
 
-    private static void ConvertFile(string fileOrDirectoryPath)
-    {
-        var ext = Path.GetExtension(fileOrDirectoryPath);
-        switch (ext.ToUpper())
+        foreach (var directoryPath in Directory.EnumerateDirectories(sourceDirectoryPath))
         {
-            case ".JPG":
-                ConvertJpeg(fileOrDirectoryPath);
-                return;
-            case ".PNG":
-                ConvertPng(fileOrDirectoryPath);
-                return;
-            default:
-                Console.WriteLine($"Extension <{ext}> is not supportted.");
-                return;
+            // a/c\
+            Console.WriteLine(directoryPath);
+            ConvertDirectory(directoryPath, Path.Combine(destinationDirectoryPath, Path.GetFileName(directoryPath)));
+        }
+        foreach (var filePath in Directory.EnumerateFiles(sourceDirectoryPath))
+        {
+            // a/b.jpg -> a.webp/b.webp
+            Console.WriteLine(filePath);
+            var destinationFilePath = Path.Combine(destinationDirectoryPath, Path.GetFileNameWithoutExtension(filePath) + ".webp");
+            ConvertFile(filePath, destinationFilePath);
         }
     }
 
-    private static void ConvertPng(string fileOrDirectoryPath)
+    // a/b.jpg, a.webp
+    private static void ConvertFile(string sourceFilePath, string destinationFilePath)
     {
-        throw new NotImplementedException();
-    }
+        WebpEncoder encoder;
+        var ext = Path.GetExtension(sourceFilePath).ToUpper();
+        switch (ext)
+        {
+            case ".JPG":
+                encoder = new WebpEncoder();
+                break;
+            case ".PNG":
+                encoder = new WebpEncoder() { FileFormat = WebpFileFormatType.Lossless };
+                break;
+            case ".WEBP":
+                Console.WriteLine($" Skip WEBP file.");
+                return;
+            default:
+                Console.WriteLine($" Extension <{ext}> is not supportted.");
+                return;
+        }
 
-    private static void ConvertJpeg(string fileOrDirectoryPath)
-    {
-        throw new NotImplementedException();
+        var image = Image.Load(sourceFilePath);
+        image.SaveAsWebp(destinationFilePath, encoder);
     }
 }
